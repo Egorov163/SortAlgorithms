37dd0e2baaec3b132ebe9753e09a34dba3ae9688
Aleksandr <Aleksandr_egorov_07@mail.ru>
2025-11-18 11:44:52 +0400

    Добавлена шейкерная сортировка.

--- AlgorithmTests/BubbleSortTests.cs
+++ AlgorithmTests/BubbleSortTests.cs
@@ -20,7 +20,7 @@ namespace Algorithm.Tests
             var rnd = new Random();
             var items = new List<int>();
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 bubble.Items.Add(rnd.Next(0, 100));
             }
---
+++ AlgorithmTests/CoctkailSortTests.cs
@@ -0,0 +1,40 @@
+﻿using Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmTests
+{
+    [TestClass]
+    public class CoctkailSortTests
+    {
+        [TestMethod]
+        public void SortTest()
+        {
+            // arrange
+            var cocktail = new CocktailSort<int>();
+
+            var rnd = new Random();
+            var items = new List<int>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                items.Add(rnd.Next(0, 100));
+            }
+
+            cocktail.Items.AddRange(items);
+            var sorted = items.OrderBy(x => x).ToArray();
+
+            // act
+            cocktail.Sort();
+
+            // assert
+            for (int i = 0; i < items.Count; i++)
+            {
+                Assert.AreEqual(sorted[i], cocktail.Items[i]);
+            }
+        }
+    }
+}
+
